58fa369e81404bf06d2b047abaad61a1c5b425fb
Sergei <kolesniksn2510@gmail.com>
2025-05-10 16:49:59 +0300

    Add lending desktop version

---
+++ script.js
@@ -0,0 +1,26 @@
+function updateArrows() {
+    const items = document.querySelectorAll(".stages-list-item");
+    let lastTop = items[0].offsetTop;
+
+    items.forEach((item, index) => {
+      const isNewLine = item.offsetTop !== lastTop;
+
+      if (index == 0) return;
+      items[index-1].classList.toggle('without-arrow', isNewLine);
+      lastTop = item.offsetTop;
+    });
+
+    items[items.length - 1].classList.toggle('without-arrow', true);
+  }
+
+window.addEventListener('resize', updateArrows);
+window.addEventListener('load', updateArrows);
+window.addEventListener("DOMContentLoaded", (event) => {
+  const dialog = document.querySelector(".modal-dialog");
+  dialog?.addEventListener('click', event => {
+      if (event.target === dialog) {
+    dialog.close();
+  }
+  });
+});
+
